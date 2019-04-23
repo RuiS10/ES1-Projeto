8def61d5f92d6d86c83aee741a790c692d921663
RuiS10 <ruimiguelsantos1999@hotmail.com>
2019-04-23 11:28:42 +0100

    Primeiro Commit

---
+++ src/Repositorio.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+public class Repositorio {
+
+    public Repositorio() {
+    }
+
+    ArrayList<Notificacao> ListaNot = new ArrayList<>();
+    ArrayList<Requisicao> ListaReq= new ArrayList<>();
+    ArrayList<Copia> ListaCop = new ArrayList<>();
+    ArrayList<Utilizador> ListaUti = new ArrayList<>();
+    ArrayList<Livro>ListaLiv= new ArrayList<>();
+    ArrayList<Emprestimo> ListaEmp= new ArrayList<>();
+    ArrayList<TipoUtilizador>ListaTipoUti= new ArrayList<>();
+    ArrayList<Devolucao> ListaDev= new ArrayList<>();
+    ArrayList<Coima> ListaCoima = new ArrayList<>();
+    ArrayList<Encomenda> ListaEnc = new ArrayList<>();
+    ArrayList<EntradaNovoLivro> ListaEntradaLiv = new ArrayList<>();
+    ArrayList<PropostaAquisicao> ListaProp = new ArrayList<>();
+    ArrayList<RequisicaoCompra> ListaReqComp = new ArrayList<>();
+
+    public void adicionaCoima(Coima coima)
+    {
+        ListaCoima.add(coima);
+    }
+    public void adicionaCopia(Copia copia) {ListaCop.add(copia);}
+    public void adicionaDevolucao(Devolucao devolucao)
+    {
+        ListaDev.add(devolucao);
+    }
+    public void adicionaEmprestimo(Emprestimo emprestimo)
+    {
+        ListaEmp.add(emprestimo);
+    }
+    public void adicionaEncomenda(Encomenda encomenda)
+    {
+        ListaEnc.add(encomenda);
+    }
+    public void EntradaNovoLivro(EntradaNovoLivro entradaNovoLivro) { ListaEntradaLiv.add(entradaNovoLivro); }
+    public void adicionaLivro(Livro livro)
+    {
+        ListaLiv.add(livro);
+    }
+    public void adicionaNotificacao(Notificacao notificacao)
+    {
+        ListaNot.add(notificacao);
+    }
+    public void adicionaPropostaAquisicao(PropostaAquisicao proposta) { ListaProp.add(proposta); }
+    public void adicionaRequisicao(Requisicao requisicao)
+    {
+        ListaReq.add(requisicao);
+    }
+    public void adicionaRequisicaoCompra(RequisicaoCompra requisicaoCompra) { ListaReqComp.add(requisicaoCompra); }
+    public void adicionaTipoUtilizador(TipoUtilizador tipo)
+    {
+        ListaTipoUti.add(tipo);
+    }
+    public void adicionaUtilizador(Utilizador utilizador)
+    {
+        ListaUti.add(utilizador);
+    }
+    public Emprestimo devolveEmprestimoDaRequisicao(Requisicao r)
+    {
+
+        for(int x = 0; x<ListaEmp.size();x++)
+        {
+            if(ListaEmp.get(x).getRequisicao()==r)
+                return ListaEmp.get(x);
+        }
+        return null;
+    }
+}
---
+++ src/RequisicaoCompraCopia.java
@@ -0,0 +1,69 @@
+import java.time.LocalDate;
+
+public class RequisicaoCompraCopia extends RequisicaoCompra{
+    private String Estado;
+    private LocalDate data;
+    private String oficio;
+    private Livro livro;
+    private Utilizador utilizador;
+
+    public RequisicaoCompraCopia(String estado, LocalDate data, String oficio, Livro livro, Utilizador utilizador,
+                                 String estado1, LocalDate data1, String oficio1, Livro livro1, Utilizador utilizador1) {
+        super(estado, data, oficio, livro, utilizador);
+        Estado = estado1;
+        this.data = data1;
+        this.oficio = oficio1;
+        this.livro = livro1;
+        this.utilizador = utilizador1;
+    }
+
+    @Override
+    public String getEstado() {
+        return Estado;
+    }
+
+    @Override
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    @Override
+    public LocalDate getData() {
+        return data;
+    }
+
+    @Override
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String getOficio() {
+        return oficio;
+    }
+
+    @Override
+    public void setOficio(String oficio) {
+        this.oficio = oficio;
+    }
+
+    @Override
+    public Livro getLivro() {
+        return livro;
+    }
+
+    @Override
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    @Override
+    public Utilizador getUtilizador() {
+        return utilizador;
+    }
+
+    @Override
+    public void setUtilizador(Utilizador utilizador) {
+        this.utilizador = utilizador;
+    }
+}
